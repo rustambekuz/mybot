1f35ddd194eb597ce6d6dd5a91372fa06119729b
rustambekuz <choriyevrustam2003@gmail.com>
2025-05-05 19:42:54 +0500

    saqla

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+logger = logging.getLogger(__name__)
 
 api_service_name = "youtube"
 api_version = "v3"
