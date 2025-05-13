1ca960548e761bfd0ea60427cd0818f7b736a151
rustambekuz <choriyevrustam2003@gmail.com>
2025-05-13 19:52:35 +0500

    Update

--- mybot.py
+++ mybot.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import sys
 from os import getenv
-from aiogram import Bot, Dispatcher, html
+from aiogram import Bot, Dispatcher, html, F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
@@ -21,8 +21,20 @@ async def command_start_handler(message: Message) -> None:
 
     await message.answer(f"Salom, {html.bold(message.from_user.full_name)}!")
 
-@dp.message(F.text=="start")
+@dp.message(F.text=="tel_user")
+async def get_me(message: Message):
+    chat_id = message.chat.id
+    full_name = message.from_user.full_name
+    text = message.text
+    username = message.from_user.username
+    await message.answer(f"Qabul qilindi! \n\nChat ID: {chat_id}\n"
+                         f"Ism: {full_name}\n"
+                         f"Username: @{username}\n"
+                         f"{text}")
 
+@dp.message(F.text.regexp(r'(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})'))
+async def check_date(message: Message):
+    await message.answer("Sizdan date formatda malumot keldi!")
 
 @dp.message()
 async def echo_handler(message: Message) -> None:
