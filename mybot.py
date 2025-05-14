f9c5632640612bea59c959097fead85e3b19f34f
rustambekuz <choriyevrustam2003@gmail.com>
2025-05-14 20:56:18 +0500

    update

--- mybot.py
+++ mybot.py
@@ -25,39 +25,56 @@ async def command_start_handler(message: Message) -> None:
     # await message.answer("Tug'ilgan yilingizni kiriting men yoshingizni aytaman!")
     # await message.answer("Yoshingizni kiriting men sizga tug'ilgan yilingizni aytaman!")
 
-@dp.message(F.text=='contact')
-async def get_me(message: Message):
-        chat_id = message.chat.id
-        full_name = message.from_user.full_name
-        text = message.text
-        username = message.from_user.username
-        nowtime = message.date
-        await message.answer(f"Qabul qilindi! \n\nChat ID: {chat_id}\n"
-                         f"Ism: {full_name}\n"
-                         f"Username: @{username}\n"
-                         f"{text}\n"
-                         f"{nowtime}")
+# @dp.message(F.text=='contact')
+# async def get_me(message: Message):
+#         chat_id = message.chat.id
+#         full_name = message.from_user.full_name
+#         text = message.text
+#         username = message.from_user.username
+#         nowtime = message.date
+#         await message.answer(f"Account malumoti! \n\nChat ID: {chat_id}\n"
+#                          f"Ism: {full_name}\n"
+#                          f"Username: @{username}\n"
+#                          f"{text}\n"
+#                          f"{nowtime}")
+#
+# @dp.message(F.text.regexp(r'(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})'))
+# async def check_date(message: Message):
+#     await message.answer("Sizdan date formatda malumot keldi!")
 
-@dp.message(F.text.regexp(r'(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})'))
-async def check_date(message: Message):
-    await message.answer("Sizdan date formatda malumot keldi!")
+import emoji
+@dp.message()
+async def handle_all(message:Message):
+    if message.text:
+        if emoji.is_emoji(message.text):
+            await message.answer("bu emoji!")
+        else:
+            await message.answer("bu matn!")
 
+    elif message.photo:
+        await message.answer("bu foto!")
 
-@dp.message(F.text)
-async def check_date(message: Message):
-    await message.answer("bu matn!")
+    elif message.document:
+        await message.answer("bu document!")
 
-@dp.message(F.photo)
-async def check_date(message: Message):
-    await message.answer("bu foto!")
+    elif message.audio:
+        await message.answer("bu audio!")
 
-@dp.message(F.video)
-async def check_date(message: Message):
-    await message.answer("bu video!")
+    elif message.video:
+        await message.answer("bu video!")
 
-@dp.message(F.audio)
-async def check_date(message: Message):
-    await message.answer("bu audio!")
+    elif message.sticker:
+        await message.answer("bu sticker!")
+
+    elif message.voice:
+        await message.answer("siz ovozli habar yubordingiz!")
+
+    elif message.animation:
+        await message.answer("bu animation!")
+
+    else:
+        await message.answer("boshqa turdagi habar!")
+
 
 
 # def calculate_age(birthdate):
@@ -66,7 +83,7 @@ async def check_date(message: Message):
 #     return today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
 #
 # REGEXP_DATE=r"(?P<day>0?[1-9]|[12][0-9]|3[01])(?P<sep>[\/\-\.])(?P<month>0?[1-9]|1[0-2])(?P=sep)(?P<year>\d{4})"
-#
+
 # @dp.message(F.text.regexp(REGEXP_DATE))
 # async def get_age_from_date(message: Message):
 #     match = re.search(REGEXP_DATE, message.text)
@@ -81,7 +98,7 @@ async def check_date(message: Message):
 #                 from datetime import datetime
 #                 birthdate = datetime(year, month, day)
 #                 age = calculate_age(birthdate)
-#                 await message.answer(f"Sizning yoshingiz: {age} da")
+#                 await message.answer(f"Sizning yoshingiz {age} da!")
 #             except ValueError:
 #                 await message.answer("Sanani noto‘g‘ri kiritdingiz.")
 #         else:
