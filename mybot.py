4dd1b664d4c585dd89af90ed2ce5b18b21cbc36a
rustambekuz <choriyevrustam2003@gmail.com>
2025-05-14 17:02:12 +0500

    update

--- mybot.py
+++ mybot.py
@@ -10,6 +10,7 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 from dotenv import load_dotenv
 
+
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
 
@@ -20,52 +21,86 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
 
-    # await message.answer(f"Salom, {html.bold(message.from_user.full_name)}! tug'ilgan yilingizni kiriting men yoshingizni aytaman!")
-    await message.answer("Tug'ilgan yilingizni kiriting men yoshingizni aytaman!")
+    await message.answer(f"Salom, {html.bold(message.from_user.full_name)}!")
+    # await message.answer("Tug'ilgan yilingizni kiriting men yoshingizni aytaman!")
+    # await message.answer("Yoshingizni kiriting men sizga tug'ilgan yilingizni aytaman!")
 
-@dp.message(F.text=="tel_user")
+@dp.message(F.text=='contact')
 async def get_me(message: Message):
-    chat_id = message.chat.id
-    full_name = message.from_user.full_name
-    text = message.text
-    username = message.from_user.username
-    await message.answer(f"Qabul qilindi! \n\nChat ID: {chat_id}\n"
+        chat_id = message.chat.id
+        full_name = message.from_user.full_name
+        text = message.text
+        username = message.from_user.username
+        nowtime = message.date
+        await message.answer(f"Qabul qilindi! \n\nChat ID: {chat_id}\n"
                          f"Ism: {full_name}\n"
                          f"Username: @{username}\n"
-                         f"{text}")
+                         f"{text}\n"
+                         f"{nowtime}")
 
-# @dp.message(F.text.regexp(r'(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})'))
-# async def check_date(message: Message):
-#     await message.answer("Sizdan date formatda malumot keldi!")
+@dp.message(F.text.regexp(r'(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})'))
+async def check_date(message: Message):
+    await message.answer("Sizdan date formatda malumot keldi!")
 
-def calculate_age(birthdate):
-    from datetime import datetime
-    today = datetime.today()
-    return today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
 
-REGEXP_DATE=r"(?P<day>0?[1-9]|[12][0-9]|3[01])(?P<sep>[\/\-\.])(?P<month>0?[1-9]|1[0-2])(?P=sep)(?P<year>\d{4})"
+@dp.message(F.text)
+async def check_date(message: Message):
+    await message.answer("bu matn!")
 
-@dp.message(F.text.regexp(REGEXP_DATE))
-async def get_age_from_date(message: Message):
-    match = re.search(REGEXP_DATE, message.text)
-    if match:
-        day_str = match.group('day')
-        month_str = match.group('month')
-        year_str = match.group('year')
+@dp.message(F.photo)
+async def check_date(message: Message):
+    await message.answer("bu foto!")
 
-        if None not in (day_str, month_str, year_str):
-            try:
-                day, month, year = int(day_str), int(month_str), int(year_str)
-                from datetime import datetime
-                birthdate = datetime(year, month, day)
-                age = calculate_age(birthdate)
-                await message.answer(f"Sizning yoshingiz: {age} yosh")
-            except ValueError:
-                await message.answer("Sanani noto‘g‘ri kiritdingiz.")
-        else:
-            await message.answer("Iltimos, sanani DD-MM-YYYY formatda yuboring.")
-    else:
-        await message.answer("Iltimos, sanani DD-MM-YYYY formatda yuboring.")
+@dp.message(F.video)
+async def check_date(message: Message):
+    await message.answer("bu video!")
+
+@dp.message(F.audio)
+async def check_date(message: Message):
+    await message.answer("bu audio!")
+
+
+# def calculate_age(birthdate):
+#     from datetime import datetime
+#     today = datetime.today()
+#     return today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+#
+# REGEXP_DATE=r"(?P<day>0?[1-9]|[12][0-9]|3[01])(?P<sep>[\/\-\.])(?P<month>0?[1-9]|1[0-2])(?P=sep)(?P<year>\d{4})"
+#
+# @dp.message(F.text.regexp(REGEXP_DATE))
+# async def get_age_from_date(message: Message):
+#     match = re.search(REGEXP_DATE, message.text)
+#     if match:
+#         day_str = match.group('day')
+#         month_str = match.group('month')
+#         year_str = match.group('year')
+#
+#         if None not in (day_str, month_str, year_str):
+#             try:
+#                 day, month, year = int(day_str), int(month_str), int(year_str)
+#                 from datetime import datetime
+#                 birthdate = datetime(year, month, day)
+#                 age = calculate_age(birthdate)
+#                 await message.answer(f"Sizning yoshingiz: {age} da")
+#             except ValueError:
+#                 await message.answer("Sanani noto‘g‘ri kiritdingiz.")
+#         else:
+#             await message.answer("Iltimos, sanani DD-MM-YYYY formatda yuboring.")
+#     else:
+#         await message.answer("Iltimos, sanani DD-MM-YYYY formatda yuboring.")
+
+# @dp.message(F.text.regexp(r'^\d{1,3}$'))
+# async def get_age_from_year(message: Message):
+#     age =int(message.text)
+#     if 1<=age<100:
+#         from datetime import datetime
+#         current_year = datetime.now().year
+#         birth_year = current_year - age
+#         await message.answer(f"Sizning tug'ilgan yilingiz: {birth_year}")
+#     else:
+#         await message.answer("Iltimos, yosh faqat 1-99 oraliqda kiriting!")
+
+
 
 @dp.message()
 async def echo_handler(message: Message) -> None:
