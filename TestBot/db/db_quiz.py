75849734115a14dfd543eb869d484eeef6e65045
rustambekuz <choriyevrustam2003@gmail.com>
2025-06-05 18:20:55 +0500

    EduTestBot dagi server xatoligi bartaraf qilindi

--- TestBot/db/db_quiz.py
+++ TestBot/db/db_quiz.py
@@ -1,35 +1,11 @@
-import psycopg2
-
-def get_connection():
-    return psycopg2.connect(
-        dbname="quiztestbot",
-        user="postgres",
-        password="1234",
-        host="localhost",
-        port="5432"
-    )
-
-
-
-import asyncpg
-
-async def get_connection_async():
-    return await asyncpg.connect(
-        user='postgres',
-        password='1234',
-        database='quiztestbot',
-        host='localhost',
-        port=5432
-    )
-
 # import psycopg2
 #
 # def get_connection():
 #     return psycopg2.connect(
-#         dbname="quiztestbot_db",
-#         user="quiztestbot_db_user",
-#         password="j1KFFJreLjozsMis9GmqilgqbsxU0IaF",
-#         host="dpg-d10m773e5dus73afg9b0-a",
+#         dbname="quiztestbot",
+#         user="postgres",
+#         password="1234",
+#         host="localhost",
 #         port="5432"
 #     )
 #
@@ -39,11 +15,35 @@ async def get_connection_async():
 #
 # async def get_connection_async():
 #     return await asyncpg.connect(
-#         user='quiztestbot_db_user',
-#         password='j1KFFJreLjozsMis9GmqilgqbsxU0IaF',
-#         database='quiztestbot_db',
-#         host='dpg-d10m773e5dus73afg9b0-a',
+#         user='postgres',
+#         password='1234',
+#         database='quiztestbot',
+#         host='localhost',
 #         port=5432
 #     )
-#
+
+import psycopg2
+
+def get_connection():
+    return psycopg2.connect(
+        dbname="quiztestbot_db",
+        user="quiztestbot_db_user",
+        password="j1KFFJreLjozsMis9GmqilgqbsxU0IaF",
+        host="dpg-d10m773e5dus73afg9b0-a",
+        port="5432"
+    )
+
+
+
+import asyncpg
+
+async def get_connection_async():
+    return await asyncpg.connect(
+        user='quiztestbot_db_user',
+        password='j1KFFJreLjozsMis9GmqilgqbsxU0IaF',
+        database='quiztestbot_db',
+        host='dpg-d10m773e5dus73afg9b0-a',
+        port=5432
+    )
+
 
